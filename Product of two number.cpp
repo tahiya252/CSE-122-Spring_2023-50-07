69903a9daab7d7cb6956b9b1b4af865bb6846e59
Tahiya Anjum Tasnim <122308843+tahiya252@users.noreply.github.com>
2023-01-24 01:49:28 +0600

    Add files via upload

---
+++ Product of two number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+double product(float num1, int num2)
+{
+    return num1*num2;
+}
+int main()
+{
+    cout<< "Product of the two numbers are: "<<product(5.5,2)<<endl;
+
+    return 0;
+}
